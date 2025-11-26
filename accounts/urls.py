a7b548ccd3ccfd7f2db9e41779df58c2ef5825e2
ElDEEB21 <omer84484@gmail.com>
2025-11-26 23:44:15 +0200

    Add home page URL pattern and enhance post card author display with avatar support

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from . import views
+from core.views import HomePageView
 
 urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='register'),
     path('login/', views.LoginView.as_view(), name='login'),
     path('logout/', views.LogoutView.as_view(), name='logout'),
     path('<str:username>/', views.UserProfileView.as_view(), name='user_profile'),
+    path('', HomePageView.as_view(), name='home')
 ]
